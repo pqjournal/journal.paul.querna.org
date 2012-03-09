e85f3f45c272b808b70bd5e47988408738c84a68
Paul Querna <pquerna@apache.org>
2012-03-10 08:40:46 +0900

    work on plugins and includes for comments

--- _plugins/github_comments.rb
+++ _plugins/github_comments.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
+require 'sanitize'
+require 'digest/md5'
+
 class Jekyll::Post
   alias :to_liquid_original :to_liquid
 
@@ -7,6 +11,12 @@ class Jekyll::Post
     Dir["#{site.source}/_comments/#{comments_slug}/*"].sort.each do |comment|
       next unless File.file?(comment) and File.readable?(comment)
       data = YAML::load_file(comment)
+      data['name'] = Sanitize.clean(data['name'])
+      data['website'] = Sanitize.clean(data['website'])
+      data['email'] = Sanitize.clean(data['email'])
+      avatar_hash = Digest::MD5.hexdigest(data['email'].downcase)
+      data['avatar_url'] = "https://secure.gravatar.com/avatar/#{avatar_hash}"
+      data['comment'] = Sanitize.clean(data['comment'], Sanitize::Config::BASIC)
       comments << data
     end
     comments
